5b46f0a6629fc5015e0f887fafedda7fd3c01867
JC <jcsmileyjr@gmail.com>
2020-03-16 09:42:18 -0500

    Attempt #3 on exercism.io Protein-translation JavaScipt challenge. Changes based on mentor suggestions.

--- protein-translation.js
+++ protein-translation.js
@@ -1,46 +1,70 @@
 const rnaToCondons = RNA => {
-    return RNA.match(/.{1,3}/g); //split string into substrings of 3
-  };
-  
-  const condonToProteins = condon => {
-    if (condon === "AUG") return "Methionine";
-    //return protein Methionine
-    else if (condon === "UUU" || condon === "UUC") return "Phenylalanine";
-    //return protein phenylalanine
-    else if (condon === "UUA" || condon === "UUG") return "Leucine";
-    //return protien Leucine
-    else if (
-      condon === "UCU" ||
-      condon === "UCC" ||
-      condon === "UCA" ||
-      condon === "UCG"
-    )
-      return "Serine";
-    //return protien Serine
-    else if (condon === "UAU" || condon === "UAC") return "Tyrosine";
-    else if (condon === "UGU" || condon === "UGC") return "Cysteine";
-    else if (condon === "UGG") return "Tryptophan";
-    else if (condon === "UAA" || condon === "UAG" || condon === "UGA")
+  return RNA.match(/.{1,3}/g); //split string into substrings of 3
+};
+
+//Check if a "STOP" sequence matches the given codon
+const ifStopSequence = condon => {
+  const stopSequence = ["UAA", "UAG", "UGA"];
+  for (let sequence of stopSequence) {
+    if (sequence === condon) {
       return "STOP";
-    //Stopping condon for DNA.
-    else throw "Invalid codon";
-  };
-  
-  export const translate = RNA => {
-    //let protein = []; //empty array to be sent if no array is passed as a parameter
-    if (RNA === undefined) {
-      return new Array(0);
     }
-  
-    let condons = rnaToCondons(RNA);
-    let aminnoAcids = []; //arrray to hold sequence of names
-  
-    for (const condon of condons) {
-      if (condonToProteins(condon) === "STOP") {
-        break;
-      }
-      aminnoAcids.push(condonToProteins(condon));
-    }
-    return aminnoAcids;
+  }
+};
+
+const condonToProteins = condon => {
+  const proteins = {
+    AUG: "Methionine",
+    UUU: "Phenylalanine",
+    UUC: "Phenylalanine",
+    UUA: "Leucine",
+    UUG: "Leucine",
+    UCU: "Serine",
+    UCC: "Serine",
+    UCA: "Serine",
+    UCG: "Serine",
+    UAU: "Tyrosine",
+    UAC: "Tyrosine",
+    UGU: "Cysteine",
+    UGC: "Cysteine",
+    UGG: "Tryptophan"
   };
+
+  let foundProtein = "";
+
+  //check each key in the lookup object against the condon, if there is a match, then mark as found
+  for (let sequence in proteins) {
+    if (condon === sequence) {
+      foundProtein = proteins[sequence];
+      break;
+    }
+  }
+
+  //if the foundProtein is stil empty, throw a error. Else return the found protein
+  if (foundProtein === "") {
+    throw "Invalid codon";
+  } else {
+    return foundProtein;
+  }
+};
+
+export const translate = RNA => {
+  //Check if the RNA is empty, if so return a empty array
+  if (RNA === undefined) {
+    return [];
+  }
+
+  let condons = rnaToCondons(RNA); //break down RNA string into an array of codons
+  let aminnoAcids = []; //arrray to hold sequence of names
+
+  //Check each condon to see: if a "stop" sequence or a "protein" sequence
+  for (const condon of condons) {
+    if (ifStopSequence(condon) === "STOP") {
+      break;
+    }
+    aminnoAcids.push(condonToProteins(condon));
+  }
+  return aminnoAcids; //return array of of proteins
+};
+
   
