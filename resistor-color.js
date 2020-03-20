7bc26358bb5577f8c79531c1d17416f26ed84f13
JC <jcsmileyjr@gmail.com>
2020-03-20 10:58:06 -0500

    Exercism.io Resistor-colors JavaScript Challenge

---
+++ resistor-color.js
@@ -0,0 +1,17 @@
+//search a array for the parameter, then return the index
+export const colorCode = color => {
+  return COLORS.indexOf(color);
+};
+
+export const COLORS = [
+  "black",
+  "brown",
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "violet",
+  "grey",
+  "white"
+];
