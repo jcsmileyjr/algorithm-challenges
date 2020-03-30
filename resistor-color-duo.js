3b65bc4a7ad9ca65cec12d3db9d9a875c1f6e731
JC Smiley <jcsmileyjr@gmail.com>
2020-03-30 10:03:35 -0500

    Attempt #2 of Exercism.io Resistor-color-duo JavaScript Challenge
    Mentor recommendation: 
    Instead of coercing the numbers returned from Array.indexOf to be strings, combining those two strings, and then coercing the resulting string to be a number, there is a way you could combine those two numbers into your return value. As a hint, don't think in terms of concatenation; think of it as an arithmetic problem. How would you combine two numbers into a single number, one representing the 'ones' value and the other representing the 'tens' portion of the final value?

--- resistor-color-duo.js
+++ resistor-color-duo.js
@@ -11,13 +11,17 @@ const colors = {
   white: 9
 };
 
+//translate the color in spot 0 and 1 into a resistor numbers, mulitply spot 0 by 10, then add all numbers
 export const decodedValue = arrayOfColors => {
-  let decodedValue1 = colors[arrayOfColors[0]]; //translate the color in spot 0 into a resistor number
-  let decodedValue2 = colors[arrayOfColors[1]]; //translate the color in spot  1into a resistor number
-  const answer = decodedValue1.toString() + decodedValue2.toString(); //Add the strings togther
-  return Number(answer); //retun the string as a number
+  return colors[arrayOfColors[0]] * 10 + colors[arrayOfColors[1]];
 };
 
-/** Alternate answer that runs but seems unreadable. Am I wrong?
+/*New alternate answer
+  let decodedValue1 = colors[arrayOfColors[0]]; //translate the color in spot 0 into a resistor number
+  let decodedValue2 = colors[arrayOfColors[1]]; //translate the color in spot  1into a resistor number
+  return decodedValue1 * 10 + decodedValue2;
+*/
+
+/** Old Alternate answer
  * return Number(colors[arrayOfColors[0]].toString() + colors[arrayOfColors[1]].toString());
  */
